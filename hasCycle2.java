262caebb80622e1faa549b64fe7b34d6cca5f956
anges-bai <45257069+anges-bai@users.noreply.github.com>
2020-03-07 16:53:04 +0800

    Create hasCycle2.java

---
+++ hasCycle2.java
@@ -0,0 +1,32 @@
+
+  Definition for singly-linked list.
+  class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+          next = null;
+      }
+  }
+ 
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head ==null || head.next == null){
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+————————————————
+版权声明：本文为CSDN博主「迷路的小绅士」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/weixin_40995778/article/details/90572972
